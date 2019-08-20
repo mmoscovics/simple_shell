0f7bf00d57000296742aeb1f1e9034c1eeec3600
computersciencebenHolberton <825@holbertonschool.com>
2019-08-20 17:07:30 +0000

    headerfile

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stay.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <string.h>
