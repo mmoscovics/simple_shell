7b826327e6312bdb61aa60ea73b64af05c8e9bd3
computersciencebenHolberton <825@holbertonschool.com>
2019-08-26 23:38:30 +0000

    strcat

---
+++ _strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ *_strcat - compares
+ *@dest:first
+ *@src:second
+ * Return:difrence 
+ */
+}
+char *strcat(char *dest, const char *src)
+{
+strcpy(dest + strlen(dest), src);
+return dest;
+}
