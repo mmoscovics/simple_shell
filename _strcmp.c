d92b6d630aa0da14828c939867e1ab863a1a826d
computersciencebenHolberton <825@holbertonschool.com>
2019-08-26 23:39:24 +0000

    strcmp

---
+++ _strcmp.c
@@ -0,0 +1,14 @@
+#include "string.h"
+/**
+ *_strcmp - cmp 2 string
+ *@s1:first string
+ *@s2:second string
+ *Return:dif 
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+int i;
+for (i = 0; s1[i] != '\0'; i++)
+if (s1[i] - s2[i] != 0)
+return (s1[i] - s2[i]);
+}
