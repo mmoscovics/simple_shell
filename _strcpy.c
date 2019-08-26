f3af22f199c39cf6b1ac03734ae9472487beb50a
computersciencebenHolberton <825@holbertonschool.com>
2019-08-26 23:40:19 +0000

    strcpy

---
+++ _strcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+/**
+ *_strcpy - copy string pointed too
+ *@dest:destination
+ *@src: source
+ *Return:To dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int n;
+for (n = 0; src[n] != '\0'; n++)
+{
+dest[n] = src[n];
+}
+dest[n] = '\0';
+return (dest);
+}
