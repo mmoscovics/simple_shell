22468b1ef211440600b3fa4e22648bfeb6908f38
Mitchell <mmoscovics@yahoo.com>
2019-08-27 20:45:02 +0000

    [mitch]update program file

--- _strcmp.c
+++ _strcmp.c
@@ -5,10 +5,12 @@
  *@s2:second string
  *Return:dif
  */
-int _strcmp(const char *s1, const char *s2)
+int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0'; i++)
-if (s1[i] - s2[i] != 0)
-return (s1[i] - s2[i]);
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
