4c7a3bdcab0c70cd2cd7577893197d0a79da36e1
Mitchell <mmoscovics@yahoo.com>
2019-08-27 00:38:18 +0000

    [mitch]update header file, create shell main file

--- holberton.h
+++ holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -6,8 +9,23 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stay.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <string.h>
+
+char *read_input(void);
+char **split_input(char *input_line);
+int _execute(char *cmdpath, char **cmdargs);
+
+char *get_path(char **envp);
+char **split_path(char *command_tokens, char *path);
+
+char *cat_tokens(char *path_tokens, char *command_tokens);
+
+int check_path(char *cmdpath);
+char *found_path(char **cmdpath);
+char *final_path(char *cmdpath);
+
+#endif /* HOLBERTON_H */
---
+++ shell_main.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * main - runs our shell
+ * @ac: argument counter
+ * @av: argument vector
+ * @envp: environment pointer
+ * Return: 0
+ */
+
+int main(int ac, char **av, char **envp)
+{
+	char *input_line, *final_path, *path;
+	char **args, **all_paths;
+	int status;
+	(void)ac;
+	(void)av;
+
+	while (1)
+	{
+		write(1, "$ ", 2);
+		input_line = read_input();
+		if (strcmp(input_line, "exit\n") == 0)
+		{
+			free(input_line);
+			exit(EXIT_SUCCESS);
+		}
+		printf("1: %s", input_line);
+		args = split_input(input_line);
+		printf("2: %s\n", *args);
+		path = get_path(envp);
+		printf("3: %s\n", path);
+		all_paths = split_path(*args, path);
+		printf("4: %s\n", *all_paths);
+		final_path = found_path(all_paths);
+		printf("5: %s\n", final_path);
+		status = _execute(final_path, args);
+		printf("6: %d\n", status);
+		free(input_line);
+		free(args);
+		free(path);
+		free(all_paths);
+		free(final_path);
+	}
+	while (status)
+		;
+	return (0);
+}
